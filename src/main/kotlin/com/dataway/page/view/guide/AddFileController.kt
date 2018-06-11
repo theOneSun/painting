656def8baf51bcace31603fa1703cf0ebd40746c
theOneSun <sunjian_better@163.com>
2018-06-11 14:32:51 +0800

    解决了表格中删除元素的问题

--- src/main/kotlin/com/dataway/page/view/guide/AddFileController.kt
+++ src/main/kotlin/com/dataway/page/view/guide/AddFileController.kt
@@ -2,30 +2,21 @@ package com.dataway.page.view.guide
 
 import com.dataway.page.model.FileData
 import com.dataway.page.primaryStageName
-import com.dataway.page.view.selfdefine.ButtonCell
 import com.dataway.page.view.selfdefine.StageManager
-import javafx.beans.value.ObservableValue
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.Scene
 import javafx.scene.control.Button
-import javafx.scene.control.Label
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
 import javafx.scene.control.cell.PropertyValueFactory
 import javafx.scene.layout.AnchorPane
 import javafx.stage.FileChooser
 import javafx.stage.Stage
-import javafx.util.Callback
 import java.io.File
 import java.net.URL
 import java.util.ResourceBundle
-import javafx.beans.property.SimpleBooleanProperty
-import com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table
-import javafx.scene.control.TableCell
 
 
 /**
@@ -40,18 +31,22 @@ class AddFileController : Initializable {
     private lateinit var nextButton: Button
     @FXML
     private lateinit var cancelButton: Button
-    //    @FXML
-//    private lateinit var fileGridPane: GridPane
-    @FXML
-    private lateinit var fileTableView: TableView<File>
-//    private lateinit var fileTableView: TableView<FileData>
 
-    private var fileList: MutableList<File> = ArrayList()
+    @FXML
+    private lateinit var fileTableView: TableView<FileData>
+
+    /**
+     * 选中的文件
+     */
+    private var fileList: MutableList<File> = arrayListOf()
+    /**
+     * 转换的可以显示的文件对象
+     */
+    private var fileDataList:MutableList<FileData> = arrayListOf()
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
 
-        //初始化gridPane
-//        initGridPane()
+        //初始化表头
         initTableView()
 
         //添加文件按钮fileList
@@ -63,12 +58,8 @@ class AddFileController : Initializable {
                 val chooserStage = Stage()
                 // 打开可以多选文件的是视窗
                 fileList = fileChooser.showOpenMultipleDialog(chooserStage)
-//                var tempFileList = fileList
-//                addFileGridPaneData(fileList)
                 if (!fileList.isEmpty()) {
-                    val fileDataList = addTableViewData()
-//                    addDeleteAction(fileDataList)
-
+                   addTableViewData()
                 }
             }
         }
@@ -105,43 +96,22 @@ class AddFileController : Initializable {
     }
 
     /**
-     * 初始化gridPane
-     */
-//    private fun initGridPane() {
-//        //初始化表格的列和表头
-//        fileGridPane.columnConstraints.addAll(ColumnConstraints().also { it.percentWidth = 40.0 }, ColumnConstraints().also { it.percentWidth = 50.0 }, ColumnConstraints().also { it.percentWidth = 10.0 })
-//        fileGridPane.rowConstraints.add(RowConstraints(30.0))
-//        fileGridPane.also {
-//            it.add(firstColumnLabel("name"), 0, 0)
-//            it.add(secondColumnLabel("where"), 1, 0)
-//        }
-//    }
-
-    /**
      * 初始化tableView
      */
     private fun initTableView() {
-//        val nameColumn = TableColumn<FileData, String>("name")
-        val nameColumn = TableColumn<File, String>("name")
+        val nameColumn = TableColumn<FileData, String>("name")
         nameColumn.prefWidth = 600.0
         nameColumn.cellValueFactory = PropertyValueFactory("name")
         nameColumn.cellValueFactory = PropertyValueFactory("name")
-        val pathColumn = TableColumn<File, String>("where")
-//        val pathColumn = TableColumn<FileData, String>("where")
+
+        val pathColumn = TableColumn<FileData, String>("where")
         pathColumn.prefWidth = 750.0
         pathColumn.cellValueFactory = PropertyValueFactory("path")
 
-//        val buttonColumn = TableColumn<File, Button>()
-//        buttonColumn.prefWidth = 150.0
-//        buttonColumn.cellValueFactory = PropertyValueFactory("deleteButton")
-//        buttonColumn.cellFactory =
-        //todo 测试新方法行不行
-        val buttonColumn = TableColumn<File, Boolean>("删除")
+        val buttonColumn = TableColumn<FileData, String>("删除")
+        buttonColumn.prefWidth = 150.0
         buttonColumn.isSortable = false
-
-        buttonColumn.setCellValueFactory({ features -> SimpleBooleanProperty(features.value != null) })
-
-        buttonColumn.setCellFactory({ ButtonCell() })
+        buttonColumn.cellValueFactory = PropertyValueFactory("deleteButton")
 
         fileTableView.columns.addAll(nameColumn, pathColumn, buttonColumn)
 
@@ -150,91 +120,35 @@ class AddFileController : Initializable {
     /**
      * 给tableView添加数据
      */
-    private fun addTableViewData() :MutableList<File>{
-//        val fileDataList: MutableList<FileData> = mutableListOf()
+    private fun addTableViewData(): MutableList<File> {
+        fileDataList.clear()
         for (file in fileList) {
-
-
-            /*deleteButton.setOnAction {
-                fileList.remove(file)
-                addTableViewData()
-            }*/
-
-//            fileDataList.add(FileData(file.name, file.path, deleteButton()))
+            fileDataList.add(FileData(file.name, file.path, deleteButton(fileList.indexOf(file))))
         }
-//        val list = FXCollections.observableArrayList(fileDataList)
-
-        fileTableView.items.also { it.clear();it.addAll(fileList) }
-
+        fileTableView.items.also { it.clear();it.addAll(fileDataList) }
         return fileList
-    }
-    /*private fun addDeleteAction(fileDataList:MutableList<File>){
-        for (fileData in fileDataList){
-            fileData.deleteButton.setOnAction {
-                fileList.removeAt(fileDataList.indexOf(fileData))
-                addTableViewData()
-            }
-        }
-    }*/
-
-    /**
-     * gridPane添加数据
-     */
-//    private fun addFileGridPaneData(fileList: MutableList<File>) {
-//        if (fileList.isEmpty()) {
-//            // 未选中文件
-//            println("未选择文件")
-//        } else {
-//            val size = fileList.size
-//            println("选择的文件的长度是$size")
-//            // 展示选中的文件
-//            var insertRow = 1
-//            for (file in fileList) {
-//                println(file)
-//                val deleteButton = deleteButton()
-//                fileGridPane.also {
-//                    it.add(firstColumnLabel(file.name), 0, insertRow)
-//                    it.add(secondColumnLabel(file.path), 1, insertRow)
-//                    it.add(deleteButton.also {
-//                        it.setOnAction {
-//                            fileList.remove(file)
-//
-//                        }
-//                    }, 2, insertRow)
-//                }
-//                insertRow++
-//            }
-//
-//        }
-//    }
-
-    /**
-     * 第一列的label
-     */
-    private fun firstColumnLabel(text: String): Label {
-        return Label(text).also {
-            it.prefWidth = 600.0
-            it.prefHeight = 30.0
-        }
-    }
-
-    /**
-     * 第二列的label
-     */
-    private fun secondColumnLabel(text: String): Label {
-        return Label(text).also {
-            it.prefWidth = 750.0
-            it.prefHeight = 30.0
-        }
     }
 
     /**
      * 删除按钮
+     * @param index 当前应该是第几行
      */
-    private fun deleteButton(): Button {
+    private fun deleteButton(index: Int): Button {
+
         return Button("删除").also {
             it.prefWidth = 150.0
             it.prefHeight = 30.0
+            it.setOnAction {
+                //设置被选中的行
+                fileTableView.selectionModel.select(index)
+                //删除对应的数据
+                //重新渲染tableView
+                val tempFileList: MutableList<File> = mutableListOf()
+                tempFileList.addAll(fileList)
+                tempFileList.removeAt(index)
+                fileList = tempFileList
+                addTableViewData()
+            }
         }
     }
 }
